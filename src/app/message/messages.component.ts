d79cd2731a2395cfe115495b7354339234a7b1de
Mateusz <mateo_pl@hotmail.com>
2020-06-08 17:29:12 +0200

    unsubscribe

--- src/app/message/messages.component.ts
+++ src/app/message/messages.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MessageService } from '../services/message.service';
 import { Message } from './message';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, OnDestroy {
 
   public messages: Message[] = [];
+  private subscription = new Subscription();
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.messageService.reciveMessages().subscribe({
-      next: (message: Message) => {
-        this.messages.push(message);
-      }
-    });
+    this.subscription.add(
+      this.messageService.reciveMessages().subscribe({
+        next: (message: Message) => {
+          this.messages.push(message);
+        }
+      })
+    );
   }
 
   removeMessage(message: Message) {
     this.messages = this.messages.filter(x => x !== message);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
